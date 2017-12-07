840d9aa913fd57b4512661fbb7c79eb19d389b0f
dccjll <yiyeshu1015@vip.qq.com>
2017-12-07 09:06:19 +0800

    更新到v1.0.3 检查国际化

--- app/src/main/java/com/yunds/pickview/MainActivity.java
+++ app/src/main/java/com/yunds/pickview/MainActivity.java
@@ -2,12 +2,35 @@ package com.yunds.pickview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.pickerview.view.TimePickerView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openTimePicker(View view) {
+        TimePickerView timePickerView = new TimePickerView(this, TimePickerView.Type.ALL);
+        timePickerView.setOnTimeSelectListener(new TimePickerView.OnTimeSelectListener() {
+            @Override
+            public void onTimeSelect(Date date) {
+                Log.i(TAG, "选择的时间：" + date);
+                ((TextView)findViewById(R.id.timeTv)).setText(new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.CHINA).format(date));
+            }
+        });
+        timePickerView.show();
     }
 }
